9c52f4815208784065cea2724c31438626d9b15c
Łukasz Cz <36801404+lukasz-cz@users.noreply.github.com>
2018-02-24 22:25:08 +0100

    Add files via upload

---
+++ KursyWalutNBP/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace KursyWalutNBP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Seria danych";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(238, 25);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Data";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(429, 379);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(121, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(60, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.Konwerter_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(187, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Odwróć";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Enabled = false;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(378, 53);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(60, 21);
+            this.comboBox2.TabIndex = 14;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.Konwerter_ValueChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(15, 51);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown1.TabIndex = 15;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.Konwerter_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Location = new System.Drawing.Point(272, 53);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.ReadOnly = true;
+            this.numericUpDown2.Size = new System.Drawing.Size(100, 20);
+            this.numericUpDown2.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 471);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Kursy walut NBP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+    }
+}
+
---
+++ KursyWalutNBP/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace KursyWalutNBP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string GetAPI(string date = "")
+        {
+            //return "[{\"table\":\"A\",\"no\":\"039 / A / NBP / 2018\",\"effectiveDate\":\"2018 - 02 - 23\",\"rates\":[{\"currency\":\"bat(Tajlandia)\",\"code\":\"THB\",\"mid\":0.1078},{\"currency\":\"dolar amerykański\",\"code\":\"USD\",\"mid\":3.3911}]}]";
+            if (date != "")
+                date = "/" + date; // tables/{table}/{date}/
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.nbp.pl/api/exchangerates/tables/A" + date + "/?format=json");
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+                return reader.ReadToEnd();
+            }
+            catch //(WebException ex)
+            {
+                return "";
+            }
+        }
+
+        public class Root
+        {
+            /*
+                "table": "A",
+                "no": "039/A/NBP/2018",
+                "effectiveDate": "2018-02-23",
+                "rates": [
+            */
+            public string table { get; set; }
+            public string no { get; set; }
+            public string effectiveDate { get; set; }
+            public IList<Rates> rates { get; set; }
+        }
+
+        public class Rates
+        {
+            /*
+                "currency": "bat (Tajlandia)",
+                "code": "THB",
+                "mid": 0.1078 
+            */
+            public string Currency { get; set; }
+            public string Code { get; set; }
+            public double Mid { get; set; }
+        }
+
+        public void LoadToTable(string JsonText)
+        {
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+
+            if (JsonText == "")
+            {
+                MessageBox.Show("Brak danych, wybierz inną datę lub typ tabeli.");
+            } else
+            {
+                JavaScriptSerializer jsonSerializer = new JavaScriptSerializer();
+                Root[] json = jsonSerializer.Deserialize<Root[]>(JsonText);
+
+                textBox1.Text = json[0].no.ToString();
+                dateTimePicker1.Value = DateTime.Parse(json[0].effectiveDate);
+                dataGridView1.DataSource = json[0].rates;
+
+                dataGridView1.Columns["Currency"].HeaderText = "Nazwa";
+                dataGridView1.Columns["Currency"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                dataGridView1.Columns["Code"].HeaderText = "Kod";
+                dataGridView1.Columns["Mid"].HeaderText = "Kurs";
+
+                comboBox1.Items.Add("PLN");
+                comboBox2.Items.Add("PLN");
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    row.Cells["Currency"].Value = row.Cells["Currency"].Value.ToString().First().ToString().ToUpper() + row.Cells["Currency"].Value.ToString().Substring(1);
+                    comboBox1.Items.Add(row.Cells["Code"].Value);
+                    comboBox2.Items.Add(row.Cells["Code"].Value);
+                }
+
+                comboBox1.Enabled = true;
+                comboBox2.Enabled = true;
+                numericUpDown1.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show(GetAPI());
+            LoadToTable(GetAPI());
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            string date = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+            LoadToTable(GetAPI(date));
+        }
+
+        private void Konwerter_ValueChanged(object sender, EventArgs e)
+        {
+            if ((comboBox1.SelectedIndex >= 0) && (comboBox2.SelectedIndex >= 0))
+            {
+                decimal kurs1 = 0, kurs2 = 0;
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (row.Cells["Code"].Value.ToString().Equals(comboBox1.Items[comboBox1.SelectedIndex].ToString()))
+                    {
+                        kurs1 = Convert.ToDecimal(row.Cells["Mid"].Value.ToString());
+                    }
+
+                    if (row.Cells["Code"].Value.ToString().Equals(comboBox2.Items[comboBox2.SelectedIndex].ToString()))
+                    {
+                        kurs2 = Convert.ToDecimal(row.Cells["Mid"].Value.ToString());
+                    }
+                }
+
+                if (comboBox1.Items[comboBox1.SelectedIndex].ToString().Equals("PLN"))
+                    kurs1 = 1;
+
+                if (comboBox2.Items[comboBox2.SelectedIndex].ToString().Equals("PLN"))
+                    kurs2 = 1;
+
+                if (kurs2 > 0) numericUpDown2.Value = Convert.ToDecimal(numericUpDown1.Value * kurs1 / kurs2);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = numericUpDown2.Value;
+            int temp = comboBox1.SelectedIndex;
+            comboBox1.SelectedIndex = comboBox2.SelectedIndex;
+            comboBox2.SelectedIndex = temp;
+        }
+    }
+}
